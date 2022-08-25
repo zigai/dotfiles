96ea5a1d8e6ea14a0ee4471fbbe37cace6175d29
zigai <ziga.ivansek@gmail.com>
2022-08-25 22:54:40 +0200

    fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,49 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting ""
+
+
+
+set -g theme_color_scheme terminal-dark
+
+alias cd.. "cd .."
+alias cd. "pwd"
+alias h "cd ~"
+alias home "cd ~"
+alias d "cd /mnt/d"
+alias e "cd /mnt/e"
+alias dw "cd ~/Downloads"
+
+alias ls 'lsd'
+alias l 'ls -a'
+alias la 'ls -a'
+alias a 'ls -la'
+alias ll "ls -la"
+alias lt 'ls --tree'
+
+alias c "clear"
+alias cl "clear"
+alias cls "clear"
+
+alias python "python3"
+alias py "python3"
+alias py3 "python3"
+alias ipy "ipython3"
+
+alias gp "git push"
+alias ga "git add ."
+alias gpl "git pull"
+alias gpl "git pull"
+alias gcm "git commit -m '$1'"
+
+alias pir "pip install -r ./requirements.txt"
+alias prq "pipreqs . --force"
+
+alias myip "curl http://ipecho.net/plain; echo"
+alias ai "sudo apt install $1"
+alias bashrc "nvim ~/.bashrc"
+alias cat 'batcat --paging=never --plain'
+alias df "df -h"
+alias ex "extract"
+alias tmxa "tmux attach-session -t $1"
